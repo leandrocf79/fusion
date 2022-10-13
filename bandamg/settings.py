7acaca5e7289e72c41132d2aa2e4fe3d7f35ad98
LEANDRO CF <83041805+leandrocf79@users.noreply.github.com>
2022-10-13 20:23:26 -0300

    Update settings.py

--- bandamg/settings.py
+++ bandamg/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-lt8*xp*z^%jpeldi7fgu^16951_em@def$&a4ysjufamn2r--e'
+SECRET_KEY = 'django-insecure-***********************************************************'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
